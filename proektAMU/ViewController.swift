7dae9aa29bc12d88ba80891bd9dc818941a43ba6
Sara Stojanoska <sarastojanoska@Saras-Mac.local>
2021-08-20 05:06:11 -0700

    Initial Commit

---
+++ proektAMU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proektAMU
+//
+//  Created by Sara Stojanoska on 8/20/21.
+//  Copyright © 2021 Sara Stojanoska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
