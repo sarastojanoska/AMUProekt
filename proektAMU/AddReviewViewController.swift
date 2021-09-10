4df6f4ca7c168d6f1cbc47e320f4598b814bfbe4
Sara Stojanoska <sarastojanoska@Saras-Mac.local>
2021-09-10 14:31:49 -0700

    reviews

---
+++ proektAMU/AddReviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddReviewViewController.swift
+//  proektAMU
+//
+//  Created by Sara Stojanoska on 9/10/21.
+//  Copyright © 2021 Sara Stojanoska. All rights reserved.
+//
+
+import UIKit
+
+class AddReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
